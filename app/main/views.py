8424fa3dddc0ed4fa6b6e261131a95563e26a0f8
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-04-30 10:14:54 +0300

    Writting functions in my views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,31 +1,46 @@
 
-
-from flask import render_template
-from app import app
-from app.requests import get_news
-
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import News, get_news,get_sources,search_news
+from .forms import ReviewForm
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-       # Getting news sources
-    source_news = get_news('sources')
-   
-    title = 'Home - Welcome to The Best Online News Hub'
-    return render_template('index.html', title = title, sources = source_news)
+    # Getting news
+    News = get_news()
+
+    title = 'Sir Felix News Hub'
+    return render_template('index.html', title = title, article = News)
+    # search_movie = request.args.get('movie_query')
+
+    # if search_movie:
+    #     return redirect(url_for('.search',movie_name=search_movie))
+    # else:
+    #     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-   
-  
-@app.route('/news/<int:news_id>')
-def news(news_id):
+@main.route('/Sources')
+def sources():
 
     '''
-    View news page function that returns the news details page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    title = f'You are viewing {news_id}'
-    return render_template('news.html',id = news_id,title = title)
+    News= get_sources()
+      
+    return render_template('news.html', Sources= News)
+
+
+# @main.route('/search/<movie_name>')
+# def search(movie_name):
+#     '''
+#     View function to display the search results
+#     '''
+#     movie_name_list = movie_name.split(" ")
+#     movie_name_format = "+".join(movie_name_list)
+#     searched_movies = search_movie(movie_name_format)
+#     title = f'search results for {movie_name}'
+#     return render_template('search.html',movies = searched_movies)
--- app/requests.py
+++ app/requests.py
@@ -1,7 +1,7 @@
 
 from app import app
 import urllib.request,json
-from .models import news
+
 
 News = news.News
 
