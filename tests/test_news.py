f9d862bfb877bc30e8be202d7789915280a87865
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-01 16:03:43 +0300

    Styling my project

--- tests/test_news.py
+++ tests/test_news.py
@@ -16,16 +16,3 @@ class SourceTest(unittest.TestCase):
         self.assertTrue(isinstance(self.new_source,Source))
 
 
-class TopHeadlinesTest(unittest.TestCase):
-    '''
-    Test Class to test the behaviour of the TopHeadlines classs
-    '''
-
-    def setUp(self):
-        '''
-        Set up method that will run before every Test
-        '''
-        self.new_TopHeadlines = ('SirFelix','BBC','Kenya got independence','/khsjha27hbs','general kenya','/khsjha27hbs','20.05.2021')
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_TopHeadlines,TopHeadlines))
