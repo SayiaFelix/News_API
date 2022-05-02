2846f9495240eb1a348c95b226d4ef186ff69a3d
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-02 18:29:41 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY='72fe65f56bb643ec8b88657d79786'
+export NEWS_API_KEY='72fe65f56bb643ec8b88657d79786962'
 export SECRET_KEY='p@zXy,HK={EhkA23Ee27,7c:]ZWU9f'
 
 python3.9 manage.py server
